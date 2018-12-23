44329d465cc87b975c08544340ee3ccd82f8d343
Katene Jack Cotton <katenejack@gmail.com>
2018-12-23 18:06:26 +1300

    adds calculations

--- calculator.js
+++ calculator.js
@@ -9,3 +9,26 @@
     // if a symbol pops up it does what that symbol says
 // if the value is less than 0 then return a negative number
 // pushes the number
+
+var inputLabel = document.getElementById('display');
+     
+    function pushButton(obj) {
+         
+        var pushed = obj.innerHTML;
+         
+        if (pushed == '=') {
+            inputLabel.innerHTML = eval(inputLabel.innerHTML);
+             
+        } else if (pushed == 'AC') {
+            
+            inputLabel.innerHTML = '0';
+             
+        } else {
+            if (inputLabel.innerHTML == '0') {
+                inputLabel.innerHTML = pushed;
+                 
+            } else {
+                inputLabel.innerHTML += pushed;   
+            }
+        }
+    }
