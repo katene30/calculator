e50ba68bb4666273639218bd2ee9fdb5ec2ba575
Katene Jack Cotton <katenejack@gmail.com>
2018-12-23 15:18:45 +1300

    adds pseudocode for calculator js

---
+++ calculator.js
@@ -0,0 +1,11 @@
+// gets the value of the button pressed
+// if the value is a number it will add to the temp string
+// if AC is recorded, it resets everything
+// if CE is recorded, it resets the last entry
+// if x is recorded, it adds a * to the entries
+// if ÷ is recorded, it adds a / to the entries
+// if = is recorded, it loops through the entries array
+    // sets next number
+    // if a symbol pops up it does what that symbol says
+// if the value is less than 0 then return a negative number
+// pushes the number
